5dd3680b45b673dd4bf7f497c2a9b3ffc05d8a75
Tom Keddie <tomk@minim.com>
2023-01-19 14:06:06 -0800

    firewall: load ip6table_raw.ko
    SW-2714

--- firewall-custom/99firewallcustom.init.d
+++ firewall-custom/99firewallcustom.init.d
@@ -8,4 +8,6 @@ boot() {
     # disable SIP nat module
     sed -i -e 's/nf_nat_sip//g' /etc/modules.d/nf-nathelper-extra
     rmmod nf_nat_sip 2>/dev/null
+    # add support for helpers over ipv6 (SW-2714)
+    insmod /lib/modules/`uname -r`/kernel/net/ipv6/netfilter/ip6table_raw.ko
 }
