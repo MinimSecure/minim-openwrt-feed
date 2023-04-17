9e580fae2adf2cddf8254ae5a044edb5faba9016
Brett Mastbergen <brettm@minim.com>
2023-04-17 16:15:31 -0400

    crash-collector: Add missing aws upload script
    SW-3947

---
+++ crash-collector/files/upload_crash_to_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# for OPENWRT_DEVICE_PRODUCT and VERSION
+. /etc/os-release
+
+filename=$1
+upload=`basename $filename`
+mac=`fw_printenv -n ethaddr | tr '[A-F]' '[a-f]'`
+
+ping -q -c 1 s3.amazonaws.com > /dev/null 2>&1
+while [ $? != 0 ] ; do
+	sleep 10
+	ping -q -c 1 s3.amazonaws.com > /dev/null 2>&1
+done
+
+curl -s --header "x-amz-acl: bucket-owner-full-control" -T $filename -X PUT \
+https://s3.amazonaws.com/crash-reports.minim.co/$OPENWRT_DEVICE_PRODUCT/$VERSION/$mac/$upload
