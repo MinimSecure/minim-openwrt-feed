547d63d3949a8d61f4f6f5819b02e8b1dcb23095
Brett Mastbergen <brettm@minim.com>
2023-04-18 14:09:48 -0400

    crash-collector: Get the correct mac addr
    Check the ethaddr and mfg_base_mac u-boot variables for the mac address
    to use when uploading kernel crashes to s3.  If we don't find either, use
    unknown
    
    SW-3947

--- crash-collector/files/upload_crash_to_s3.sh
+++ crash-collector/files/upload_crash_to_s3.sh
@@ -7,6 +7,14 @@ filename=$1
 upload=`basename $filename`
 mac=`fw_printenv -n ethaddr | tr '[A-F]' '[a-f]'`
 
+if [ -z $mac ] ; then
+	mac=`fw_printenv -n mfg_base_mac | tr '[A-F]' '[a-f]'`
+fi
+
+if [ -z $mac ] ; then
+	mac="unknown"
+fi
+
 ping -q -c 1 s3.amazonaws.com > /dev/null 2>&1
 while [ $? != 0 ] ; do
 	sleep 10
